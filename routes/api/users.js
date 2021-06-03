b79a70cb28e90c27485bc37e4b418bf53c7e1f37
Tewodros Girma <TeGirma@excellerentsolutions.com>
2021-06-03 20:21:30 +0300

    route-created

---
+++ routes/api/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const  router = express.Router();
+const {check, validationResult}= require('express-validator/check');
+
+// @route       POST api/users
+// @description Register User
+// @access      Public
+
+
+router.post('/',[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email','please include a valid email').isEmail(),
+    check('password','Please enter a password with   or more characters').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors: errors.array()});
+    }
+    console.log(req.body);
+    res.send('User route')
+});
+
+module.exports = router;
