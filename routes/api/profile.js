c2d64d3d78aaccde0565e22e0b0f7f57a3846451
Tewodros Girma <68501568+Teddy-GH@users.noreply.github.com>
2021-06-10 05:40:46 +0300

    Add,Get and Delete Posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -346,33 +346,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
  // @description Get  user repos from Github
  // @access      Public
 
-route.get('/github/:username', (req, res) => {
-    try {
-        const options = {
-    uri: `https://api.github.com/users/${
-        req.params.username
-        }/repos?per_page=5&
-        sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=$
-        {config.get('githubSecret')}`,
-            method: 'GET',
-            headers: { 'user-agent': 'node.js' }
-        };
+//    route.get('/github/:username', (req, res) => {
+//     try {
+//         const options = {
+//     uri: `https://api.github.com/users/${
+//         req.params.username
+//         }/repos?per_page=5&
+//         sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=$
+//         {config.get('githubSecret')}`,
+//             method: 'GET',
+//             headers: { 'user-agent': 'node.js' }
+//         };
 
-        request(options, (error, response, body) => {
-            if (error) console.log(error);
+//         request(options, (error, response, body) => {
+//             if (error) console.log(error);
 
-            if (response.statusCode !== 200) {
-                return res.status(404).json({ msg: 'No Github profile found!' });
-            }
-            res.json(JSON.parse(body));
-        });
+//             if (response.statusCode !== 200) {
+//                 return res.status(404).json({ msg: 'No Github profile found!' });
+//             }
+//             res.json(JSON.parse(body));
+//         });
     
-} catch (err) {
-    console.log(err.message);
-    res.status(500).send('Server error')
-}
+// } catch (err) {
+//     console.log(err.message);
+//     res.status(500).send('Server error')
+// }
     
-});
+// });
 
 
 module.exports = router;
